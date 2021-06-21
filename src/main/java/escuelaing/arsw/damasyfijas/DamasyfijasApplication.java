fef762f52de70551e1924f4a16c03f7a56a0d039
Jose1102 <jose.castroortega1102@gmail.com>
2021-06-20 21:24:10 -0500

    AAdd repository

---
+++ src/main/java/escuelaing/arsw/damasyfijas/DamasyfijasApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.arsw.damasyfijas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamasyfijasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamasyfijasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/arsw/damasyfijas/DamasyfijasApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.arsw.damasyfijas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamasyfijasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
